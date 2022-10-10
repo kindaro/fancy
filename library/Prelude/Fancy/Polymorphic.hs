e06088ab591710fb780af4224b936179c4c4f04d
Ignat Insarov <kindaro@gmail.com>
2022-10-10 20:03:27 +0300

    Format.

--- library/Prelude/Fancy/Polymorphic.hs
+++ library/Prelude/Fancy/Polymorphic.hs
@@ -19,9 +19,11 @@ import Prelude.Fancy.Polymorphic.Polymorph (Polymorph (..), Polymorphic (..))
 import Prelude.Fancy.Polymorphic.Shrivel (Shrivel (..), Shrivelling (..))
 import Prelude.Fancy.Polymorphic.Warp (Warp (..), Warping (..))
 
--- | Compose nested functors everywhere in a tuple list. This lets you then
--- apply a natural transformation.
+{- | Compose nested functors everywhere in a tuple list. This lets you then
+ apply a natural transformation.
+-}
 class Weld sundered welded | sundered → welded, welded → sundered where weld ∷ sundered → welded
+
 instance Weld () () where weld _ = ()
 instance
   ( Weld sundereds weldeds
@@ -31,20 +33,32 @@ instance
   weld (sundered :× sundereds) = W sundered :× weld sundereds
 
 -- | Apply a tuple list of functions to a matching tuple list of values.
-class Bristle arrows sources targets
-  | arrows → sources, arrows → targets, sources targets → arrows
-  where bristle ∷ arrows → sources → targets
-instance Bristle ( ) ( ) ( ) where bristle ( ) ( ) = ( )
-instance Bristle arrows sources targets ⇒ Bristle ((source → target) × arrows) (source × sources) (target × targets)
-  where bristle (arrow :×  arrows) (source :× sources ) = arrow source :× bristle arrows sources
+class
+  Bristle arrows sources targets
+    | arrows → sources
+    , arrows → targets
+    , sources targets → arrows
+  where
+  bristle ∷ arrows → sources → targets
+
+instance Bristle () () () where bristle () () = ()
+instance Bristle arrows sources targets ⇒ Bristle ((source → target) × arrows) (source × sources) (target × targets) where
+  bristle (arrow :× arrows) (source :× sources) = arrow source :× bristle arrows sources
 
 -- | Turn a functor of matching tuple lists into a tuple list of functors.
 type family Transposed outer tuple = result where
-  Transposed functor ( ) = ( )
+  Transposed functor () = ()
   Transposed functor (value, transposable) = (functor value, Transposed functor transposable)
-class transposed ~ Transposed outer tuple ⇒ TransposeTuple outer tuple transposed
-  | outer tuple → transposed where transposeTuple ∷ outer tuple → transposed
-instance TransposeTuple functor ( ) ( ) where transposeTuple _= ( )
-instance (TransposeTuple functor transposable transposed, Functor functor)
-  ⇒  TransposeTuple functor (value, transposable) (functor value, transposed)
-  where transposeTuple functor = (fmap fst functor, (transposeTuple ∘ fmap snd) functor)
+
+class
+  transposed ~ Transposed outer tuple ⇒
+  TransposeTuple outer tuple transposed
+    | outer tuple → transposed
+  where
+  transposeTuple ∷ outer tuple → transposed
+instance TransposeTuple functor () () where transposeTuple _ = ()
+instance
+  (TransposeTuple functor transposable transposed, Functor functor) ⇒
+  TransposeTuple functor (value, transposable) (functor value, transposed)
+  where
+  transposeTuple functor = (fmap fst functor, (transposeTuple ∘ fmap snd) functor)
